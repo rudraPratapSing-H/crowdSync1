65cf09cf7c11d5acdfec633e8ae034ba981ddfc3
Rudra Pratap Singh <24bcs156@ietdavv.edu.in>
2025-09-08 16:54:55 +0530

    added firebase credentials in env file

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import time
 import random
+import os
 import firebase_admin
 from firebase_admin import credentials, db
 
-# Firebase setup
-cred = credentials.Certificate("serviceAccountKey.json")  # download from Firebase
+# Load Firebase credentials from environment variables
+firebase_creds = {
+    "type": os.environ.get("FIREBASE_TYPE"),
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN"),
+}
+
+cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://crowdmanagement-e36d0-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    'databaseURL': os.environ.get('FIREBASE_DATABASE_URL')
 })
 
 zones = ["A", "B", "C", "D", "E", "F"]
